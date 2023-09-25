1fa29b4bf80ad7cbaf1b21a28145705450a3c0c6
acer <acer@10.0.0.107>
2023-09-25 15:21:47 +0530

    commit

---
+++ project1/src/topics/Concat.java
@@ -0,0 +1,12 @@
+package topics;
+
+public class Concat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String n1="hello";
+   String n2="java";
+	System.out.println("the string:" + (n1+n2));
+	}
+
+}
