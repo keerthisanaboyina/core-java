17cca7bcb2fa98e9272862ec95de5e2c89687c0f
acer <acer@192.168.1.58>
2023-10-06 11:42:07 +0530

    nine

--- Javaproject/src/com/tnsif/interface2/Amazon.java
+++ Javaproject/src/com/tnsif/interface2/Amazon.java
@@ -1,13 +1,17 @@
 package com.tnsif.interface2;
 
+import java.util.Scanner;
+
 public class Amazon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int amt;
+		
        Accounts a=new Prime();
        Accounts a1=new NonPrime();
-       a.deliveryCharge(10);
-       a1.deliveryCharge(200);
+       a.deliveryCharge(0);
+       a1.deliveryCharge(1000);
        }
 
 }
