30d6810cc9f02048154eb2a1cda2c939d80b5370
acer <acer@192.168.1.35>
2023-10-09 09:14:48 +0530

    Tenth commit

--- Javaproject/src/com/tnsif/interface2/Amazon.java
+++ Javaproject/src/com/tnsif/interface2/Amazon.java
@@ -6,12 +6,10 @@ public class Amazon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int amt;
-		
        Accounts a=new Prime();
        Accounts a1=new NonPrime();
-       a.deliveryCharge(0);
-       a1.deliveryCharge(1000);
+       a.deliveryCharge();
+       a1.deliveryCharge();
        }
 
 }
