4a5735450d675e55842167154bbccf8ac12e8c1b
acer <acer@192.168.1.35>
2023-10-09 09:21:02 +0530

    Ninth commit

--- Javaproject/src/com/tnsif/interface2/Prime.java
+++ Javaproject/src/com/tnsif/interface2/Prime.java
@@ -8,11 +8,4 @@ public class Prime implements Accounts {
 		int amt=0;
 		System.out.println("the delivery charge for prime account :"+amt);
 	}
-
-	@Override
-	public void deliveryCharge(int n) {
-		// TODO Auto-generated method stub
-		System.out.println("the delivery charge for prime account :"+n);
-	}
-
 }
