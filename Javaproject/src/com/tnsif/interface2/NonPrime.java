8659434969ca68b4c17bef970540f46708c22f01
acer <acer@192.168.1.35>
2023-10-09 09:18:31 +0530

    Ninth commit

--- Javaproject/src/com/tnsif/interface2/NonPrime.java
+++ Javaproject/src/com/tnsif/interface2/NonPrime.java
@@ -9,10 +9,4 @@ public class NonPrime  implements Accounts {
 		System.out.println("the delivery charge for nonprime account :"+amt);
 	}
 
-	@Override
-	public void deliveryCharge(int n) {
-		// TODO Auto-generated method stub
-		System.out.println("the delivery charge for nonprime account :"+n);
-	}
-
 }
