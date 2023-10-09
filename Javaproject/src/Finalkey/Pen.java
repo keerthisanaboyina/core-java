2900b691735495af620ed6c0ec30e5f1fbb813cf
acer <acer@192.168.1.35>
2023-10-09 09:56:20 +0530

    12th commit

---
+++ Javaproject/src/Finalkey/Pen.java
@@ -0,0 +1,17 @@
+package Finalkey;
+
+final class Pen {
+	final void display(int a)
+	   {
+		   System.out.println("value   :"+ a);// TODO Auto-generated method stub
+	   }
+	
+	public static void main(String[] args) {
+	  final  int a=10;
+	   
+	   Pen in=new Pen();
+      in.display(a)	;   
+	   
+	}
+
+}
