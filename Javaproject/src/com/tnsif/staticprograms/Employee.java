439310e2e4e9b5aa5466066b27da7c7417fd51ac
acer <acer@10.0.0.100>
2023-10-10 14:54:05 +0530

    13th commit

--- Javaproject/src/com/tnsif/staticprograms/Employee.java
+++ Javaproject/src/com/tnsif/staticprograms/Employee.java
@@ -4,6 +4,22 @@ public class Employee {
 
 	private String  name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	private  int id;
 	
 	private String companyname="TNS";
--- Javaproject/src/com/tnsif/staticprograms/EmployeeDemo.java
+++ Javaproject/src/com/tnsif/staticprograms/EmployeeDemo.java
@@ -4,7 +4,7 @@ public class EmployeeDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee ob= new Employee("keerthi",28);
+		Employee ob= new Employee("keerthi",3);
 		System.out.println(ob);
 	}
 }
