601756f287ea0ccb8b000417b64aa3f09c78e2d1
acer <acer@10.0.0.100>
2023-10-10 14:32:07 +0530

    13th commit

---
+++ Javaproject/src/com/tnsif/staticprograms/Employee.java
@@ -0,0 +1,28 @@
+package com.tnsif.staticprograms;
+
+public class Employee {
+
+	private String  name;
+
+	private  int id;
+	
+	private String companyname="TNS";
+	
+	
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + ", companyname=" + companyname + "]";
+	}
+	
+}
---
+++ Javaproject/src/com/tnsif/staticprograms/EmployeeDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.staticprograms;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee ob= new Employee("keerthi",28);
+		System.out.println(ob);
+	}
+}
