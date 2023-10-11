7507510fcd6b52c719730bcc0b536c2249bbf5cd
acer <acer@10.0.0.108>
2023-10-11 08:49:06 +0530

    15th commit

---
+++ Javaproject/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.encapsulation;
+
+import com.tnsif.project.Encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj = new Encapsulation();
+		obj.serialno=4;
+		obj.name="keerthi";
+		obj.age=19;
+		obj.show();
+	}
+
+}
